d42786c43917bf6aab0e48d83efdbc93fc5f1835
sv <svadivazhagu@wpi.edu>
2025-01-22 02:08:16 -0800

    overhauled parsing and added content blocking (x.com) for specific servers

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import discord
 import os
 import time
 
-intents = discord.Intents(messages=True)
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True  
 
-bot = discord.Bot(intents=discord.Intents.all())
+bot = discord.Bot(intents=intents)
 
-def embed(message:str):
+def embed(message: str):
     aliased = None
     if "twitter.com" in message and "vxtwitter" not in message and 'status' in message:
         aliased = message.replace("twitter.com", "vxtwitter.com")
@@ -14,28 +16,50 @@ def embed(message:str):
         aliased = message.replace("x.com", "vxtwitter.com")
     elif "instagram.com" in message and "instagramez.com" not in message and ('reels' in message or 'reel' in message):
         aliased = message.replace("instagram.com", "instagramez.com")
-    #Tikok embed replacement no longer needed as Discord does it right
-    # elif "tiktok.com" in message and "tiktxk" not in message:
-    #     aliased = message.replace("tiktok", "tiktxk")
     return aliased
-    
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    e = None
-    e = embed(message.content)
-    if e:
-        await message.delete()
 
-    with open("embed.log", 'a') as file:
-        now = time.time()
-        file.write(f"{now}-{message.author.name}-{aliased}\n") 
+    # Check if the message is from RSC
+    if message.guild and message.guild.name == "Rush Site C":
+        if "x.com" in message.content or "twitter.com" in message.content:
+            try:
+                await message.delete()
+                await message.author.send(
+                    f"Hi {message.author.name}, your message comes from a bad place (`twitter`).")
+            except discord.Forbidden:
+                with open("embed.log", 'a') as file:
+                    now = time.time()
+                    file.write(f"Could not send a DM to {message.author.name}.")
+                    file.close()
 
-    print(f"{message.author.name} shared {aliased}")
-    await message.channel.send(message.author.mention + " shared " + aliased)
-    return
+
+
+    aliased = embed(message.content)
+
+    if aliased:
+        #Handle the scenario for SFPJs
+        #Check if the channel is not 
+        if message.guild.name == "Squad Footie Pajamas" and message.channel != "brainrot":
+
+            target_channel = discord.utils.get(message.guild.channels, name="brainrot")  # Replace 'log-channel' with your target channel name
+            if target_channel:
+                await target_channel.send(f"{message.author.display_name} shared: {aliased}")
+                await message.delete()
+        
+        
+        else:
+            await message.channel.send(message.author.display_name + " shared: " + aliased)
+        
+        with open("embed.log", 'a') as file:
+            now = time.time()
+            file.write(f"{now}-{message.author.name}-{message.content}\n")
+            file.close()
+
+
 
 discord_key = os.environ.get('discord')
 
